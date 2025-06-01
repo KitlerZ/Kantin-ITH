4b8adc719f389793c2290a2c993304ba56316064
FirmanV02 <firmanjgy@gmail.com>
2025-06-01 13:39:00 +0800

    Update check_menu_table.php

--- backend/check_menu_table.php
+++ backend/check_menu_table.php
@@ -7,20 +7,17 @@ include 'db.php';
 header('Content-Type: application/json');
 
 try {
-    // Check if table exists
     $result = $conn->query("SHOW TABLES LIKE 'menu'");
     if ($result->num_rows === 0) {
         throw new Exception("Table 'menu' does not exist");
     }
 
-    // Get table structure
     $result = $conn->query("DESCRIBE menu");
     $columns = [];
     while ($row = $result->fetch_assoc()) {
         $columns[] = $row;
     }
 
-    // Get sample data
     $result = $conn->query("SELECT * FROM menu LIMIT 1");
     $sample = $result->fetch_assoc();
 
